c76e7d0829b4f60117406650e962b2e5e7053a21
zh997 <2565272484@qq.com>
2021-06-15 23:35:27 +0800

    feat: bug

---
+++ android/app/src/main/kotlin/com/nmmhbstl/egou/wxapi/WXEntryActivity.kt
@@ -0,0 +1,11 @@
+package com.nmmhbstl.egou.wxapi;
+
+import android.os.Bundle
+import android.widget.Toast
+import com.jarvan.fluwx.wxapi.FluwxWXEntryActivity
+
+class WXEntryActivity : FluwxWXEntryActivity() {
+  override fun onCreate(@Nullable savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+  }
+}
